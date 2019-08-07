e233ff475f629c8c6d8397f106b81f6911fed29f
zwang190 <savarov890418@gmail.com>
2019-08-06 17:11:36 -0700

    new.

--- component/src/index.js
+++ component/src/index.js
@@ -2,11 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import faker from 'faker'
 import CommentDetail from './CommentDetail'
+import ApprovalCard from './ApprovalCard'
 
 const App = () => {
     return (
         <div className="ui container comments">
+            <ApprovalCard />
             <CommentDetail author='Sam' timeAgo='13:39pm' content="Nice blog post" avatar={faker.image.avatar()}/>
+            <CommentDetail author='John' timeAgo='13:00pm' content="Nice blog post" avatar={faker.image.avatar()}/>
         </div>
     )
 };
